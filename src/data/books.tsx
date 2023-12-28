60a7aa77f77191429de54381dde0e2c8ef39fe0c
Keyur Golani <keyur.golani@brevitaz.com>
2023-12-29 01:28:34 +0530

    Add users and books mock data

---
+++ src/data/books.tsx
@@ -0,0 +1,152 @@
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  publicationYear: number;
+  genre: string;
+}
+
+const books: Book[] = [
+  {
+    id: 1,
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    publicationYear: 1960,
+    genre: "Fiction",
+  },
+  {
+    id: 2,
+    title: "1984",
+    author: "George Orwell",
+    publicationYear: 1949,
+    genre: "Dystopian",
+  },
+  {
+    id: 3,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    publicationYear: 1925,
+    genre: "Classics",
+  },
+  {
+    id: 4,
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    publicationYear: 1951,
+    genre: "Fiction",
+  },
+  {
+    id: 5,
+    title: "To the Lighthouse",
+    author: "Virginia Woolf",
+    publicationYear: 1927,
+    genre: "Modernist",
+  },
+  {
+    id: 6,
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel Garcia Marquez",
+    publicationYear: 1967,
+    genre: "Magical Realism",
+  },
+  {
+    id: 7,
+    title: "Brave New World",
+    author: "Aldous Huxley",
+    publicationYear: 1932,
+    genre: "Dystopian",
+  },
+  {
+    id: 8,
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    publicationYear: 1813,
+    genre: "Romance",
+  },
+  {
+    id: 9,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    publicationYear: 1937,
+    genre: "Fantasy",
+  },
+  {
+    id: 10,
+    title: "The Road",
+    author: "Cormac McCarthy",
+    publicationYear: 2006,
+    genre: "Post-Apocalyptic",
+  },
+  {
+    id: 11,
+    title: "Frankenstein",
+    author: "Mary Shelley",
+    publicationYear: 1818,
+    genre: "Gothic",
+  },
+  {
+    id: 12,
+    title: "Lord of the Flies",
+    author: "William Golding",
+    publicationYear: 1954,
+    genre: "Adventure",
+  },
+  {
+    id: 13,
+    title: "The Shining",
+    author: "Stephen King",
+    publicationYear: 1977,
+    genre: "Horror",
+  },
+  {
+    id: 14,
+    title: "The Chronicles of Narnia",
+    author: "C.S. Lewis",
+    publicationYear: 1950,
+    genre: "Fantasy",
+  },
+  {
+    id: 15,
+    title: "Moby-Dick",
+    author: "Herman Melville",
+    publicationYear: 1851,
+    genre: "Adventure",
+  },
+  {
+    id: 16,
+    title: "Harry Potter and the Philosopher's Stone",
+    author: "J.K. Rowling",
+    publicationYear: 1997,
+    genre: "Fantasy",
+  },
+  {
+    id: 17,
+    title: "The Odyssey",
+    author: "Homer",
+    publicationYear: 800,
+    genre: "Epic Poetry",
+  },
+  {
+    id: 18,
+    title: "Wuthering Heights",
+    author: "Emily Brontë",
+    publicationYear: 1847,
+    genre: "Gothic",
+  },
+  {
+    id: 19,
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    publicationYear: 1979,
+    genre: "Science Fiction",
+  },
+  {
+    id: 20,
+    title: "Crime and Punishment",
+    author: "Fyodor Dostoevsky",
+    publicationYear: 1866,
+    genre: "Philosophical Fiction",
+  },
+];
+
+export default books;
---
+++ src/data/users.tsx
@@ -0,0 +1,22 @@
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+const users: User[] = [
+    { id: 1, firstName: "Keyur", lastName: "Patel", email: "keyurpatel@example.com", password: "abc123" },
+    { id: 2, firstName: "Pooja", lastName: "Sharma", email: "pooja@example.com", password: "abc123" },
+    { id: 3, firstName: "Rahul", lastName: "Gupta", email: "rahul@example.com", password: "abc123" },
+    { id: 4, firstName: "Sneha", lastName: "Verma", email: "sneha@example.com", password: "abc123" },
+    { id: 5, firstName: "Kiran", lastName: "Singh", email: "kiran@example.com", password: "abc123" },
+    { id: 6, firstName: "Anita", lastName: "Yadav", email: "anita@example.com", password: "abc123" },
+    { id: 7, firstName: "Vikas", lastName: "Joshi", email: "vikas@example.com", password: "abc123" },
+    { id: 8, firstName: "Suman", lastName: "Mishra", email: "suman@example.com", password: "abc123" },
+    { id: 9, firstName: "Raj", lastName: "Kumar", email: "raj@example.com", password: "abc123" },
+    { id: 10, firstName: "Neha", lastName: "Saxena", email: "neha@example.com", password: "abc123" },
+];
+
+export default users;
